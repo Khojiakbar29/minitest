58cb44ca7fdb136dd492dba6b4cecef8fee5db71
Khojiakbar <kh.tukhtasinov@gmail.com>
2022-08-04 15:04:23 +0100

    Timer is Ready

--- script.js
+++ script.js
@@ -5,37 +5,45 @@ function mainFunction () {
 
         if (userName) {
             document.getElementById('username').innerHTML = `${userName}, nice to see you`;
-        } else if (userName == null) {
+        } else if (userName == null || userName == '') {
             document.getElementById('username').innerHTML = `Please enter your name :)`;
         }
     }
 
     setTimeout(nameIdentify, 3000);
 
-    if (userName) {
+    if (userName != undefined) {
         let changeStatus = function() {
-            document.getElementById('maintext').innerHTML = `Are you ready? ${userName}?`; 
+            document.getElementById('maintext').innerHTML = `Are you ready, ${userName}?`; 
         };
 
-        setTimeout(changeStatus, 5000);
+        setTimeout(changeStatus, 8000);
+
     }
-        // function changeState() {
-        //     let sec;
-        //     document.getElementById('maintext').innerHTML = `Are you ready? ${userName}?`;
-        //     function init() {
-        //         sec = 0;
-        //         setInterval(tick, 1000);
-        //     }
-        //     init();   
-        //     function tick() {
-        //         while(sec <= 5){
-        //             sec++;
-        //         }
-        //         document.getElementById('username').innerHTML = `Test will start in ${sec}`;
-        //     }
-            
-        // }
-        // setTimeout(changeState, 7000);
+
+    function changeState() {
+        let sec = 10;
+        let t;
+        function tick() {
+            sec-=1;
+        }
+        function add() {
+            tick();
+            document.getElementById('username').innerHTML = `Test will start in ${sec}`;
+            timer();
+        }
+        function timer() {
+            t = setTimeout(add, 1000);
+            if (sec == 0) {
+                clearTimeout(t);
+            }
+        }
+        timer();
+        
+    }
+    
+    setTimeout(changeState, 7000);
+
 
 
 }
